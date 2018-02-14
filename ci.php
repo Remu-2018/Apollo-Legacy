69bd8d5379e82b576d687f6c7db62c7f447f4d6d
Kyoko-Kirigiri <wesleyvanneck1012@gmail.com>
2018-02-14 18:56:06 +0000

    Update ci.php

--- ci.php
+++ ci.php
@@ -12,7 +12,7 @@ $server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --d
 	1 => ["pipe", "w"],
 	2 => ["pipe", "w"]
 ], $pipes);
-fwrite($pipes[0], "\nversion\nms\nstop\n");
+fwrite($pipes[0], "version\nms\nstop\n");
 while(!feof($pipes[1]) and time()-$time<60*3){
 	echo fgets($pipes[1]);
 }
